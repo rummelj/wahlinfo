a5d9bda354715742ddbe53c9640d4f2055ed1497
cg <constantin.gerstberger@gmail.com>
2012-12-14 10:17:28 +0100

    created sql files to aggregate Votes

---
+++ wahlinfo/src/main/resources/sql/aggregateDirCanVotes.sql
@@ -0,0 +1,11 @@
+﻿WITH VotesPerCandidate as (
+	SELECT	fvp.directCandidateId, count(*) as receivedVotes
+	FROM	WIFilledVotingPaper fvp
+	GROUP BY fvp.directCandidateId
+)
+UPDATE 	WIDirectCandidate dc
+SET	receivedVotes = vpc.receivedVotes
+FROM	VotesPerCandidate vpc	
+INNER JOIN  WIDirectCandidate dc1
+ON	dc1.id 	= vpc.directCandidateId
+WHERE	dc.id	= dc1.id
---
+++ wahlinfo/src/main/resources/sql/aggregatePartyVotes.sql
@@ -0,0 +1,10 @@
+﻿DELETE FROM	WIPartyVotes;
+
+INSERT INTO	WIPartyVotes (federalStateId, partyId, receivedVotes)
+
+SELECT	ed.federalStateId, fvp.partyId, count(*) as receivedVotes
+FROM 	WIFilledVotingPaper fvp, WIElectoralDistrict ed
+WHERE	fvp.electoralDistrictId	= ed.number	AND
+	fvp.partyId		is not null
+GROUP BY ed.federalStateId, fvp.partyId
+;
