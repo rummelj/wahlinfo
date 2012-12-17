3e040f54eebd0a22172935e8cdd6b6f9e6753742
cg <constantin.gerstberger@gmail.com>
2012-12-17 19:34:02 +0100

    minor change towards backend

--- wahlinfo/src/main/java/com/tu/wahlinfo/frontend/model/SyncModel.java
+++ wahlinfo/src/main/java/com/tu/wahlinfo/frontend/model/SyncModel.java
@@ -1,11 +1,13 @@
 package com.tu.wahlinfo.frontend.model;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.tu.wahlinfo.analysis.IVoteAnalysis;
 import com.tu.wahlinfo.csv.CsvParserException;
 import com.tu.wahlinfo.csv.parser.ICsvToDatabaseSyncer;
 import com.tu.wahlinfo.persistence.DatabaseException;
@@ -18,12 +20,15 @@ public class SyncModel {
 
 	@Inject
 	ICsvToDatabaseSyncer csvToDatabaseSyncer;
+	
+	@Inject
+	IVoteAnalysis voteAnalysis;
 
-	public void launchSync() throws CsvParserException, DatabaseException {
+	public void launchSync() throws CsvParserException, DatabaseException,
+			IOException {
 		File[] voteFiles = csvToDatabaseSyncer.sync();
-		for (int k = 0; k < voteFiles.length; k++) {
-			csvToDatabaseSyncer.sync2(voteFiles[k], k);
-		}
+		csvToDatabaseSyncer.sync2(voteFiles);
+		voteAnalysis.updateVoteBase();
 	}
 
 }
