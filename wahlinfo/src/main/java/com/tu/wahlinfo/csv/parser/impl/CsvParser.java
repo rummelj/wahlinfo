c47b79221f173ac8fce5ba1fcb7c733bbb0361fc
cg <cg@localhost>
2013-01-01 06:19:57 -0800

    Removed redundant parts of vote generation from parser

--- wahlinfo/src/main/java/com/tu/wahlinfo/csv/parser/impl/CsvParser.java
+++ wahlinfo/src/main/java/com/tu/wahlinfo/csv/parser/impl/CsvParser.java
@@ -212,28 +212,23 @@ public class CsvParser implements ICsvParser {
                     break;
                 }
                 tmpAggregation = new CsvVoteAggregation(reader.get(0));
-                for (int k = 11; k < reader.getHeaderCount(); k += 4) {
+                for (int k = 19; k < reader.getHeaderCount(); k += 4) {
                     int index = k;
-                    if (index == 15) {
-                        // skip entry for valid votes
-                        continue;
-                    }
+                    
                     partyName = reader.getHeader(index).split(
                             AFFILIATION_DELIMITER)[0];
                     partyId2005 = this
                             .getPartyId(ElectionYear._2005, partyName);
                     partyId2009 = this
                             .getPartyId(ElectionYear._2009, partyName);
-                    // first iteration is invalid votes -> null values
-                    if (index != 11) {
-                        directCandidateId2005 = this.sortedDirectCandidates2005
-                                .get(Long.parseLong(reader.get(0))).get(
-                                partyId2005);
-                        directCandidateId2009 = this.sortedDirectCandidates2009
-                                .get(Long.parseLong(reader.get(0))).get(
-                                partyId2009);
-                    }
-
+                    
+                    directCandidateId2005 = this.sortedDirectCandidates2005
+                             .get(Long.parseLong(reader.get(0))).get(
+                             partyId2005);
+                    directCandidateId2009 = this.sortedDirectCandidates2009
+                             .get(Long.parseLong(reader.get(0))).get(
+                             partyId2009);
+                    
                     tmpAggregation.addPartialVoteAggregation(
                             directCandidateId2005, directCandidateId2009,
                             partyId2005, partyId2009, reader.get(index + 1),
